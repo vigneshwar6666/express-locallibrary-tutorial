175a58d980a0d56efcf9f1754ca9be543898dfb7
vigneshwar6666 <vigneshwar6666@gmail.com>
2020-04-10 15:12:25 -0500

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://Vigneshwar:<Vignesh4321>@cluster0-b1p7s.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
