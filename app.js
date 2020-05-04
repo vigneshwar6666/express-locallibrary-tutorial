18aa032617e6c731a3590f6cb13fb9f6db8dced1
vigneshwar6666 <vigneshwar6666@gmail.com>
2020-05-03 21:14:29 -0500

    commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 dotenv.config({ path: '.env' })
 const mongoose = require('mongoose');
 
-var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var dev_db_url = 'mongodb+srv://Vigneshwar:Vignesh4321@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
 
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
